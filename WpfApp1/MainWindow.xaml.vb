2826e8060dea34fb046151d8399fc10618d9deef
Minh Nhat Huynh <Minh Nhat Huynh@DINH-HT>
2021-07-22 19:26:15 +0700

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        WB.Navigate("D:\Dinh\id.html")
+
+    End Sub
+End Class
